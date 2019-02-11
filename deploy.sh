e3ae7976d9e0d4a947a71bedbafbeeba46de40e0
CA\prabhnarora <prabhnarora@deloitte.ca>
2019-02-11 17:48:35 -0500

    Updated docker repos

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t prabhneetdocker/multi-client:latest -t prabhneetdocker/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t prabhneetdocker/multi-server:latest -t prabhneetdocker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t prabhneetdocker/multi-worker:latest -t prabhneetdocker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push prabhneetdocker/multi-client:latest
+docker push prabhneetdocker/multi-server:latest
+docker push prabhneetdocker/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push prabhneetdocker/multi-client:$SHA
+docker push prabhneetdocker/multi-server:$SHA
+docker push prabhneetdocker/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=prabhneetdocker/multi-server:$SHA
+kubectl set image deployments/client-deployment client=prabhneetdocker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=prabhneetdocker/multi-worker:$SHA
